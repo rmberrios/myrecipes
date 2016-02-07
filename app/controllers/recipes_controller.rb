28664c513a6716d3c774f8dad3fc9308252da551
Rommel Berrios <rommel.berrios93@gmail.com>
2016-02-07 18:10:38 +0000

    Add feature show recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,10 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+  
+  def show
+    # Stop server and check pry
+    # binding.pry
+    @recipe = Recipe.find(params[:id])
+  end
 end
