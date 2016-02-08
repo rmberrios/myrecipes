7dc64e906de9e6c677c963ecf6be58bf901898fc
Rommel Berrios <rommel.berrios93@gmail.com>
2016-02-08 19:29:21 +0000

    Add features edit recipe, forms and partials

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   ### Recipes routes
   get '/recipes', to: 'recipes#index'
   get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
-  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
   post '/recipes', to: 'recipes#create'
+  get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+  patch '/recipes/:id', to: 'recipes#update'
+  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
   
-  # get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
-  # patch '/recipes/:id', to: 'recipes#update'
+  
   # delete '/recipes/:id', to: 'recipes#destroy'
   
   ## all the code above can be compress into 
