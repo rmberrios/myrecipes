f7a2abf4a51f55c3940b89a890db728d09e52851
Rommel Berrios <rommel.berrios93@gmail.com>
2016-02-08 04:03:11 +0000

    Add feature create recipe

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   ### Recipes routes
   get '/recipes', to: 'recipes#index'
+  get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
   get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+  post '/recipes', to: 'recipes#create'
   
-  # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
-  # post '/recipes', to: 'recipes#create'
   # get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
   # patch '/recipes/:id', to: 'recipes#update'
   # delete '/recipes/:id', to: 'recipes#destroy'
