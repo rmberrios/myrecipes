a0fce69195c587221868cb0e17c2c0370d1ceaff
Rommel Berrios <rommel.berrios93@gmail.com>
2016-02-17 18:07:37 +0000

    Set Edit-New Recipe to add Style and Ingredients

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -56,7 +56,7 @@ class RecipesController < ApplicationController
   private 
     def recipe_params
       params.require(:recipe)
-      .permit(:name, :summary, :description, :picture)
+      .permit(:name, :summary, :description, :picture, style_ids: [], ingredient_ids: [])
     end
     
     def set_recipe 
